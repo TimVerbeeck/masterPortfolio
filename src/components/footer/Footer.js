0c027f3782c60b15a0e2965eef66ef6a87cd6945
TimVerbeeck <tim.lucario@gmail.com>
2024-04-18 18:34:06 +0200

    change: footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -9,7 +9,7 @@ export default function Footer(props) {
     <div className="footer-div">
       <Fade>
         <p className="footer-text" style={{ color: props.theme.secondaryText }}>
-          2024©, made by {greeting.title}
+          made by {greeting.title} (2024©)
         </p>
         {/* <ToggleSwitch theme={props.theme} onToggle={props.onToggle}/> */}
       </Fade>
